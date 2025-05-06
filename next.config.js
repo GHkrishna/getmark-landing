58844c6845c6e5fdb34c5291589b19ae642c84f5
Krishna Waske <waskekrishna@gmail.com>
2025-05-06 08:26:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -10,4 +9,4 @@ const nextConfig = {
   trailingSlash: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
