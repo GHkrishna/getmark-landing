f34b95e2b33c06f8c56052a8ac260a47d5720d79
GHkrishna <waskekrishna@gmail.com>
2025-05-12 08:14:58 +0530

    fix: landing
    Signed-off-by: GHkrishna <waskekrishna@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/test/",
+  base: "/getmark-landing/",
   server: {
     host: "::",
     port: 8080,
